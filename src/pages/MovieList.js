3ac0f88db9f4dc9256987ea5dccf63fd01c4d93d
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-25 17:15:47 +0700

    Created Custom Hook to request different urls

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,21 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useFetch } from '../hooks/useFetch';
 import { Card } from '../components';
 
-export const MovieList = () => {
-  
-  const [ movies, setMovies ] = useState([]);
-  
-  
-  useEffect(() => {
-    const API_KEY = process.env.REACT_APP_API_KEY
-
-    async function fetchMovies(){
-      const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`);
-      const data = await response.json();
-      setMovies(data.results);
-    }
-    fetchMovies();
-  },[]);
+export const MovieList = ({apiPath}) => {
+  const { data: movies } = useFetch(apiPath);
 
   return (
     <main>
