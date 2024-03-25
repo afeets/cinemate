55e1dc7692eedc64f57ce3df891ccc70646c5105
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-25 15:45:12 +0700

    fetchQuery to url, stored state. Mapped list passing to Card Component for render

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,14 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Card } from '../components';
 
 export const MovieList = () => {
+  
+  const [ movies, setMovies ] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies(){
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=5fdd760607ed1dd9ec2d2610da60bdee");
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  },[]);
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          { movies.map((movie) => (
+            <Card key={ movie.id } movie={movie}/>
+          ))}
         </div>
       </section>
     </main>
