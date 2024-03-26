ea782723fcdda016f9946250be4fb9b3d0f1e94f
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-26 15:51:52 +0700

    with useLocation create component that scrolls to top of page change of url

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { Header } from "./Header";
 export { Footer } from "./Footer";
+export { ScrollToTop } from "./ScrollToTop";
 
 export { Card } from "./Card";
 export { Button } from "./Button";
